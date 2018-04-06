9b81f8113bc21289a3234bc6779f6bf3df01b194
Mansour Alharthi <malharthi9@gatech.edu>
2018-04-06 01:40:24 -0400

    Revised interprocedural def-use analysis .. moderately tested..

--- infeasible_paths/InterproceduralDemandDrivenDefUseRun.cpp
+++ infeasible_paths/InterproceduralDemandDrivenDefUseRun.cpp
@@ -24,10 +24,20 @@ namespace {
 
 
 			map<string, set<pair<BasicBlock*, BasicBlock*>>>  def_use;
-
+			set<string> localVar; 
 			for(BasicBlock& B : F)
-				InterproceduralDemandDrivenDefUse().startBlockAnalysis(B, *m, def_use);
+				InterproceduralDemandDrivenDefUse().startBlockAnalysis(B, *m, def_use, localVar);
 
+
+			map<string, set<pair<BasicBlock*, BasicBlock*>>>::iterator it;
+			for (it = def_use.begin(); it != def_use.end(); ++it){
+					errs() << "\t[$] Def-Use(" << it->first << "): ";
+
+					for(pair<BasicBlock*, BasicBlock*> p : it->second)
+						errs() << "(" << p.first->getParent()->getName() << ":" << p.first->getName() << ", " << p.second->getParent()->getName() << ":" << p.second->getName() << ") ";
+
+					errs() << "\n"; 
+			}
 
       return false;
     }
